0df120b9b348159b4da73c1f99cea06119cef610
Ilnur <emaililnur@gmail.com>
2025-07-03 22:58:02 +0300

    add postgre

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,11 +7,11 @@ import { AppService } from './app.service';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: 'Q427001q',
-      database: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5433'),
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'Q427001q',
+      database: process.env.DB_NAME || 'postgres',
       entities: [],
       synchronize: true,
     }),
